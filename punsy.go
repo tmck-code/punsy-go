81b4c110461f641dd611a88164d0f21934606b42
Tom McKeesick <tmck01@gmail.com>
2019-08-21 12:33:08 +1000

    Use CLI args to search

--- punsy.go
+++ punsy.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/tmck-code/punsy-go/src/dictionary"
 )
 
 func main() {
 	// fileUrl := "http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b"
 	// dictionary.DownloadFile(fileUrl, ofpath)
+    args := os.Args[1:]
 
 	c := dictionary.NewCMU()
 	ofpath := "cmudict-0.7b.utf8"
 	dictionary.LoadCMU(ofpath, c)
 
-	msg := "world"
+	msg := args[0]
 	result := c.GetPronunciation(msg)
 	fmt.Println(result)
+	if rhyme, ok := c.Rhymes.Get(result); ok {
+		fmt.Println(rhyme.Data))
+	}
 }
